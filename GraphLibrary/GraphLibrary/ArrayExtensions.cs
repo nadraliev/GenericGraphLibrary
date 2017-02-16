40114f96ac932f9125bab4d7d81bd0c67a5f0f72
nadraliev <soutvoid@gmail.com>
2017-02-16 22:53:52 +0300

    init commit. added base functionality

---
+++ GraphLibrary/GraphLibrary/ArrayExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphLibrary
+{
+    public static class ArraysUtils
+    {
+        public static T[,] ResizeArray<T>(T[,] original, int rows, int columns)
+        {
+            var newArray = new T[rows, columns];
+            int minRows = rows;
+            int minColumns = columns;
+            if (original != null)
+            {
+                minRows = Math.Min(rows, original.GetLength(0));
+                minColumns = Math.Min(columns, original.GetLength(1));
+            }
+            for (int i = 0; i < minRows; i++)
+                for (int j = 0; j < minColumns; j++)
+                    if (original != null)
+                        newArray[i, j] = original[i, j];
+                    else newArray[i, j] = default(T);
+            return newArray;
+        }
+
+        public static T[,] TrimArray<T>(int rowToRemove, int columnToRemove, T[,] originalArray)
+        {
+            T[,] result = new T[originalArray.GetLength(0) - 1, originalArray.GetLength(1) - 1];
+
+            for (int i = 0, j = 0; i < originalArray.GetLength(0); i++)
+            {
+                if (i == rowToRemove)
+                    continue;
+
+                for (int k = 0, u = 0; k < originalArray.GetLength(1); k++)
+                {
+                    if (k == columnToRemove)
+                        continue;
+
+                    result[j, u] = originalArray[i, k];
+                    u++;
+                }
+                j++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GraphLibrary/GraphLibrary/Graph.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphLibrary
+{
+    public class Graph<V,E>
+    {
+        private IList<V> vertexes;
+        private E[,] edges;
+
+        public Graph() {}
+        public Graph(IList<V> vertexes) { this.vertexes = vertexes; edges = new E[vertexes.Count, vertexes.Count]; }
+        public Graph(IList<V> vertexes, E[,] edges) { this.vertexes = vertexes; this.edges = edges; }
+
+        public V GetVertexAt(int index)
+        {
+            return vertexes[index];
+        }
+
+        public void SetVertexAt(int index, V vertex)
+        {
+            vertexes[index] = vertex;
+        }
+
+        public void AddVertex(V vertex)
+        {
+            vertexes.Add(vertex);
+            edges = ArraysUtils.ResizeArray<E>(edges, edges.GetLength(0) + 1, edges.GetLength(1) + 1);
+        }
+
+        public void RemoveVertexAt(int index)
+        {
+            vertexes[index] = default(V);
+            edges = ArraysUtils.TrimArray(index, index, edges);
+        } 
+
+        public E GetEdge(int from, int to)
+        {
+            return edges[from, to];
+        }
+
+        public void SetEdge(int from, int to, E edge)
+        {
+            edges[from, to] = edge;
+        }
+
+        public void ClearEdge(int from, int to)
+        {
+            edges[from, to] = default(E);
+        }
+
+        public void ClearAllEdges()
+        {
+            for (int i = 0; i < edges.GetLength(0); i++)
+                for (int k = 0; k < edges.GetLength(1); k++)
+                    ClearEdge(i, k);
+        }
+
+        public bool IsEdgeSet(int from, int to)
+        {
+            return edges[from, to].Equals(default(E));
+        }
+
+        public void ClearGraph()
+        {
+            vertexes = null;
+            edges = null;
+        }
+
+        public override string ToString()
+        {
+            string result = String.Empty;
+            result += vertexes.GetType().Name;
+            vertexes.ToList<V>().ForEach(x => result += x.ToString() + " ");
+            result += Environment.NewLine + edges.GetType().Name;
+            for (int i = 0; i < edges.GetLength(0); i++)
+            {
+                result += Environment.NewLine;
+                for (int k = 0; k < edges.GetLength(1); k++)
+                    result += i + "," + k + ":" + edges[i,k].ToString() + " ";
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(vertexes, edges).GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Graph<V,E>))
+                return false;
+            else
+                return vertexes.Equals(((Graph<V, E>)obj).vertexes) && edges.Equals(((Graph<V, E>)obj).edges);
+        }
+    }
+}
